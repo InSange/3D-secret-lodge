ba77d1a9fcc7b6ea47360ce1d1d9a905e20227da
InSange <ehfrur888@nvaer.com>
2024-03-06 04:43:43 +0900

    Add NPC Type
    this type use to Dialogue

--- team-2/Assets/Scripts/NPC.cs
+++ team-2/Assets/Scripts/NPC.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum NPC
+{
+    Boy = 0,
+    Man,
+    Dwarf,
+    Robin,
+    Woman
+}
+
 public class NPC : MonoBehaviour
 {
     
